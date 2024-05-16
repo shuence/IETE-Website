35b810beac921fe725796cf5b9927b73aa4ade6b
shuence <Shubhampitekar98@gmail.com>
2024-05-16 12:10:45 +0530

    Updated Home Page

--- src/components/AboutInst.js
+++ src/components/AboutInst.js
@@ -1,109 +1,141 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from "react-bootstrap";
-import deims from "../assets/diemslogoiete.png"
-import Button from 'react-bootstrap/Button';
-import director from "../assets/Director.png"
-import hod from "../assets/Hod.png"
-import dean from "../assets/dean.png"
-import Card from "react-bootstrap/Card";
-
+import React, { Component } from "react";
+import { Container, Row, Col, Card, Button, Carousel } from "react-bootstrap";
+import diems1 from "../assets/diemslogoiete.png";
+import naac from "../assets/nacclogo.png";
+import nba from "../assets/nba.png";
+import director from "../assets/Director.png";
+import hod from "../assets/Hod.png";
+import dean from "../assets/dean.png";
 
 class AboutInst extends Component {
-    render() {
-        return (
-            <Container fluid className="about-section-one ">
-                <Container>
-                    <Row style={{ justifyContent: "center", padding: "10px" }}>
-                        <h1 className="diems-heading" style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                            About Deogiri Institute of Engineering and Management Studies
-                        </h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      authorityData: [
+        {
+          imageSrc: hod,
+          title: "Dr. R. M. Autee",
+          description: "Head of E&TC Department",
+        },
+        {
+          imageSrc: dean,
+          title: "Dr. S. V. Lahane",
+          description: "Dean Academics",
+        },
+        {
+          imageSrc: director,
+          title: "Dr. Ulhas Shiurkar",
+          description: "Director",
+        },
+      ],
+      slideshowImages: [diems1, naac, nba],
+    };
+  }
 
-                        <Col
-                            md={8}
-                            style={{
-                                justifyContent: "center",
-                                paddingTop: "30px",
-                                paddingBottom: "50px",
+  render() {
+    const { authorityData, slideshowImages } = this.state;
 
-                            }}
-                        >
-
-                            <blockquote className="blockquote ">
-                                <p style={{ textAlign: "justify", paddingTop: "50px" }}><span className="purple">Deogiri Institute of Engineering and Management Studies </span>
-
-                                    is affiliated to the Dr. Babasaheb Ambedkar Technological University, Lonere and is developed as per the norms of the All India Council of Technical Education (A.I.C.T.E.), Government of India.The Institute is consistently among the top institutes of Dr. B.A.M. University in examination results. The student-centric teaching learning environment, state of the art infrastructure, well-equipped laboratories, library stacked with good number of books, experienced and dedicated team of faculties are the key features of the Institute.
-
-                                    <br />
-                                    <br />
-                                </p>
-                            </blockquote>
-                            <Button className="button-core-team" target="_blank"
-                                href={"https://www.dietms.org/"}
-                                style={{
-                                    display: "flex", justifyContent: "center", alignItems: "center", marginLeft: "30%",
-                                    marginRight: "30%"
-                                }}>
-                                Visit Official DIEMS page
-                            </Button>
-
-
-                        </Col>
-                        <Col
-                            style={{
-                                paddingTop: "80px", paddingBottom: "50px", 
-                            }}
-                            className="myAvtar"
-
-                        >
-                            <img src={deims}
-                                alt="about"
-                                className="img-fluid main-iete-logo"
-                                style={{ maxHeight: "300px" }}
-                            />
-                        </Col>
-
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Card className="authority-card">
-                                <Card.Img variant="top" src={hod} alt="card-img" />
-                                <Card.Body>
-                                    <Card.Title style={{ textAlign: "center" }}>Dr. R. M. Autee</Card.Title>
-                                    <Card.Text style={{ textAlign: "center" }}>
-                                        Head of E&TC Department 
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="authority-card">
-                                <Card.Img variant="top" src={dean} alt="card-img" />
-                                <Card.Body>
-                                    <Card.Title style={{ textAlign: "center" }}>Dr. S. V. Lahane</Card.Title>
-                                    <Card.Text style={{ textAlign: "center" }}>
-                                        Dean Academics
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-
-                        <Col md={4}>
-                            <Card className="authority-card">
-                                <Card.Img variant="top" src={director} alt="card-img" />
-                                <Card.Body>
-                                    <Card.Title style={{ textAlign: "center" }}>Dr. Ulhas Shiurkar</Card.Title>
-                                    <Card.Text style={{ textAlign: "center" }}>
-                                        Director
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-
-                    </Row>
-                </Container>
-            </Container>
-        );
-    }
+    return (
+      <Container fluid className="about-section-one">
+        <Container>
+          <Row style={{ justifyContent: "center", padding: "10px" }}>
+            <h1
+              className="diems-heading"
+              style={{ fontSize: "2.1em", paddingBottom: "20px" }}
+            >
+              About Deogiri Institute of Engineering and Management Studies
+            </h1>
+            <Col
+              md={8}
+              style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
+            >
+              <blockquote className="blockquote">
+                <p style={{ textAlign: "justify", paddingTop: "50px" }}>
+                  <span className="purple">
+                    Deogiri Institute of Engineering and Management Studies{" "}
+                  </span>
+                  is affiliated to the Dr. Babasaheb Ambedkar Technological
+                  University, Lonere and is developed as per the norms of the
+                  All India Council of Technical Education (A.I.C.T.E.),
+                  Government of India. The Institute is consistently among the
+                  top institutes of Dr. B.A.M. University in examination
+                  results. The student-centric teaching learning environment,
+                  state of the art infrastructure, well-equipped laboratories,
+                  library stacked with a good number of books, experienced and
+                  dedicated team of faculties are the key features of the
+                  Institute.
+                  <br />
+                  <br />
+                </p>
+              </blockquote>
+              <Button
+                className="button-core-team"
+                target="_blank"
+                href={"https://www.dietms.org/"}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginLeft: "30%",
+                  marginRight: "30%",
+                }}
+              >
+                Visit Official DIEMS page
+              </Button>
+            </Col>
+            <Col
+              style={{ paddingTop: "20px", paddingBottom: "50px" }}
+              className="myAvtar"
+            >
+              <Carousel showArrows={true} showIndicators={true} slide={true}>
+                {slideshowImages.map((image, index) => (
+                  <Carousel.Item key={index} style={{ textAlign: "center" }}>
+                    <img
+                      src={image}
+                      alt={`Slide ${index + 1}`}
+                      className="img-fluid main-iete-logo"
+                      style={{ maxHeight: "300px" }}
+                    />
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+            </Col>
+          </Row>
+          <Row>
+            {authorityData.map((authority, index) => (
+              <Col key={index}>
+                <Card className="authority-card">
+                  <Card.Img
+                    variant="top"
+                    style={{
+                      display: "block",
+                      marginInline: "auto",
+                      marginTop: "20px",
+                      width: "50%",
+                    }}
+                    src={authority.imageSrc}
+                    alt="card-img"
+                  />
+                  <Card.Body>
+                    <Card.Title style={{ textAlign: "center" }}>
+                      {authority.title}
+                    </Card.Title>
+                    <Card.Text style={{ textAlign: "center" }}>
+                      {authority.description}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </Container>
+    );
+  }
 }
 
 export default AboutInst;
--- src/components/RecentEvents.js
+++ src/components/RecentEvents.js
@@ -1,37 +1,71 @@
-import React, {  } from "react";
-import { Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom"
-import Team_Collaboration from "../assets/Team_Collaboration.svg"
-
+import React from "react";
+import { Container, Row, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import event1Image from "../assets/events/inaugration2.png";
+import event2Image from "../assets/events/blogstar.png";
+import event3Image from "../assets/events/workshop.jfif";
+import event4Image from "../assets/events/gateMock.png";
+import event5Image from "../assets/events/mockInterview.png";
 
 function RecentEvents() {
-    return (
-        <section>
-            <Container fluid className="recent-section">
-                <Container>
-                    <Row className="home-about-description">
-                        <h2 className="heading-name-recent">Check out our recent events!</h2>
-                        <img
-                            src={Team_Collaboration}
-                            alt="Nothing to show here"
-                            className="img-fluid"
-                            style={{
-                                maxHeight: "332px", marginTop: "70px", marginBottom: "50px", display: "block",
-                                marginLeft: "auto",
-                                marginRight: "auto"
-                            }}
-                        />
-                        <p className="recent-para" style={{ textAlign: "center", fontSize: "1.3em", padding: "20px" }}>
-                            Here's a sneak peek into our recent events. Click the button below to explore more.
-                        </p>
-                        <Link to="/events">
-                            <button className="button-core-team" style={{padding:'15px',color:'white',borderRadius:'5px'}} target="_blank">Go to Events</button>
-                        </Link>
-                    </Row>
-                </Container>
-            </Container>
-        </section>
-    );
+  const events = [
+    {
+      image: event1Image,
+      title: "DIEMS IETE Student Forum Inauguration Program",
+      details: "Join us on 27th Oct 2023 for the IETE Forum Inauguration.",
+    },
+    {
+      image: event2Image,
+      title: "IETE BlogStar Challenge",
+      details: "Submit your tech-related blog for a chance to win!",
+    },
+    {
+      image: event3Image,
+      title: "Micromaster Workshop",
+      details: "Attend our two-day workshop on the 8051 microcontroller.",
+    },
+    {
+      image: event4Image,
+      title: "Mock GATE Exam",
+      details: "Simulate the GATE exam environment and assess your readiness.",
+    },
+    {
+      image: event5Image,
+      title: "Mock Interviews",
+      details: "Refine your interview skills with insights from industry experts.",
+    },
+  ];
+
+  return (
+    <section>
+      <Container fluid className="recent-section">
+        <Container>
+          <Row className="home-about-description">
+            <h2 className="heading-name-recent">Check out our recent events!</h2>
+            <Carousel className="carousel-container" slide={true}>
+              {events.map((event, index) => (
+                <Carousel.Item key={index} style={{ textAlign: "center" }}>
+                  <img
+                    className="d-block mx-auto"
+                    src={event.image}
+                    alt={event.title}
+                    style={{ width: "80%", height: "auto" }}
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+
+            <p className="recent-para" style={{ textAlign: "center", fontSize: "1.3em", padding: "20px" }}>
+              Here's a sneak peek into our recent events. Click the button below to explore more.
+            </p>
+            <Link to="/events">
+              <button className="button-core-team" style={{ padding: '15px', color: 'white', borderRadius: '5px' }} target="_blank">Go to Events</button>
+            </Link>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
 }
 
 export default RecentEvents;
